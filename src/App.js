5f49cb114ea6e62680ce040afeef7dda8d36dca9
dmrozik87 <dmrozik87@gmail.com>
2023-02-28 13:56:27 +0100

    Foodlist state moved to App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
-import './App.css';
-import Header from './components/Header';
-import About from './components/About';
-import WhoAreYou from './components/WhoAreYou';
-import Foodlist from './components/Foodlist';
-import Contact from './components/Contact';
+import "./App.css";
+import { useState } from "react";
 
+import Header from "./components/Header";
+import About from "./components/About";
+import WhoAreYou from "./components/WhoAreYou";
+import Foodlist from "./components/Foodlist";
+import Contact from "./components/Contact";
 
 function App() {
+  const [foodDonation, setFoodDonation] = useState({});
+  const [listOfAllAlreadyAddedFoodDonations, setListOfAllAlreadyAddedFoodDonations] = useState([]);
+
+  function handleInput(event, category) {
+    setFoodDonation({
+      category: category,
+      quantity: parseInt(event.target.value),
+    });
+  }
+
+  function findIndexOfAlreadyAddedFood(category) {
+    return listOfAllAlreadyAddedFoodDonations.findIndex(
+      (food) => food.category === category
+    );
+  }
+
+  function handleClick(category) {
+    if (findIndexOfAlreadyAddedFood(category) !== -1) {
+      listOfAllAlreadyAddedFoodDonations[
+        findIndexOfAlreadyAddedFood(category)
+      ].quantity = parseInt(foodDonation.quantity);
+      setListOfAllAlreadyAddedFoodDonations(listOfAllAlreadyAddedFoodDonations);
+      return;
+    }
+    setListOfAllAlreadyAddedFoodDonations((prevList) => [
+      ...prevList,
+      foodDonation,
+    ]);
+  }
+
   return (
     <div className="App">
       <Header />
       <About />
       <WhoAreYou />
-      <Foodlist />
+      <Foodlist        
+        handleClick={handleClick}
+        handleInput={handleInput}
+      />
       <Contact />
     </div>
   );
--- src/components/Foodlist.js
+++ src/components/Foodlist.js
@@ -1,40 +1,10 @@
-import { useEffect, useState } from "react";
 import food from './food.js'
 
-
-function Foodlist() {
-
-    const [foodList, setFoodList] = useState(food);
-    const [foodDonation, setFoodDonation] = useState({})
-    const [listOfAllAlreadyAddedFoodDonations, setListOfAllAlreadyAddedFoodDonations] = useState([])
-
-    function handleInput(event, category) {
-        setFoodDonation({
-            category: category,
-            quantity: parseInt(event.target.value)
-        })
-    }
-
-    function findIndexOfAlreadyAddedFood(category) {
-        return listOfAllAlreadyAddedFoodDonations.findIndex(food => food.category === category)
-    }
-
-    function handleClick(category) {
-        if (findIndexOfAlreadyAddedFood(category) !== -1) {
-            listOfAllAlreadyAddedFoodDonations[findIndexOfAlreadyAddedFood(category)].quantity += parseInt(foodDonation.quantity)
-            setListOfAllAlreadyAddedFoodDonations(listOfAllAlreadyAddedFoodDonations)
-            return;
-        }
-        setListOfAllAlreadyAddedFoodDonations(prevList => [...prevList, foodDonation])
-    }
-
-    useEffect(() => {
-         console.log(listOfAllAlreadyAddedFoodDonations);
-     }, [foodDonation, listOfAllAlreadyAddedFoodDonations]); 
+function Foodlist(props) {
 
     return (
         <div className="foodlist">
-            {foodList.map(food => (
+            {food.map(food => (
                 <div className="foodCard">
                     <img className="food" src={food.img} alt="" />
                         <h1 className="category">{food.category}</h1>
@@ -46,8 +16,8 @@ function Foodlist() {
                                 min="1"
                                 max="100"
                                 placeholder={food.quantity}
-                                onChange={(event) => handleInput(event, food.category)} />
-                            <button className={food.category} onClick={() => handleClick(food.category)}>ADD</button>
+                                onChange={(event) => props.handleInput(event, food.category)} />
+                            <button className={food.category} onClick={() => props.handleClick(food.category)}>ADD</button>
                         </div>
                     </div>
             ))}
