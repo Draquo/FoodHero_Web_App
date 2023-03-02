e6badfd7c7d41b1ff7e6ba601b774fa51a36cf5d
Jovi-dr <jowita.druciak@gmail.com>
2023-03-02 16:53:19 +0100

    TypingTitle component

---
+++ src/components/WhoAreYou/TypingTitle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import TypingText from 'react-text-typing-animation'
+
+
+const TypingTitle = () => {
+  return (
+    <TypingText className="typingTitle" text={"Join our foundation in the fight against hunger!"} speed={1.5} delay={5000} />
+  )
+}
+
+export default TypingTitle
